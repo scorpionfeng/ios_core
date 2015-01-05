0f8a6695bbc998ad3f3cdd53affe4d9043eec463
ken <ken@kentekiMac-mini.local>
2015-01-05 21:33:32 +0800

    Initial Commit

---
+++ BlueCon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueCon
+//
+//  Created by ken on 15-1-5.
+//  Copyright (c) 2015年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueCon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueCon
+//
+//  Created by ken on 15-1-5.
+//  Copyright (c) 2015年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
